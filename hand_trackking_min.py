f7235c5039d7deaa3b091e553611016a335ee69b
callum thomas <callumthomas086@gmail.com>
2021-09-05 11:13:54 +0100

    x

--- hand_trackking_min.py
+++ hand_trackking_min.py
@@ -9,12 +9,18 @@ cap = cv2.VideoCapture(0)
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
+mpdraw =mp.solutions.drawing_untils
+
 
 while True:
     succsess, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results  = hands.process(imgRGB)
-    print (results.multi_hand.landmarks)
+    # print (results.multi_hand._landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_landmarks:
+            mpdraw.draw_landmarks(img,handLms)
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
