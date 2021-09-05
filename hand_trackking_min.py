35257b5d3969085dcc76b7883c04765ca23aff04
callum thomas <callumthomas086@gmail.com>
2021-09-05 12:01:29 +0100

    base

--- hand_trackking_min.py
+++ hand_trackking_min.py
@@ -27,6 +27,8 @@ while True:
                 h, w, c = img.shape
                 cx, cy = int(lm.x*w), int(lm.y*h)
                 print( cx, cy)
+                if id == 0 :
+                    cv2.circle(img, (cx,cy) , 15, (255, 0, 255), cv2.FILLED)
             mpdraw.draw_landmarks(img,handLms, mpHands.HAND_CONNECTIONS)
 
     cTime = time.time()
