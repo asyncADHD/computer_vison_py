b966a3c68de29598efe2d7ce4ddb3d6f7d16d1b2
callum thomas <callumthomas086@gmail.com>
2021-09-05 11:22:26 +0100

    d

--- hand_trackking_min.py
+++ hand_trackking_min.py
@@ -10,6 +10,8 @@ cap = cv2.VideoCapture(0)
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpdraw =mp.solutions.drawing_utils
+pTime = 0
+cTime = 0
 
 
 while True:
@@ -21,6 +23,12 @@ while True:
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             mpdraw.draw_landmarks(img,handLms, mpHands.HAND_CONNECTIONS)
+
+    cTime = time.time()
+    fps = 1/(cTime-pTime)
+    pTime = cTime
+
+    cv2.putText(img,str(int(fps)),(10,70), cv2.FONT_HERSHEY_PLAIN,3,(255, 0, 255), 3)
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
